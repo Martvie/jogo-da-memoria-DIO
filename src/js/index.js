2b73b03b1dff33c5f07229a332eacca826792dd2
Martvie <marviedias@gmail.com>
2022-12-15 15:44:25 -0300

    unflip function

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,45 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
-function flipCard(){
+function flipCard() {
+
+    if(lockBoard) return;
+
     this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkCard();
+}
+
+function checkCard() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disbleCards();
+    } else {
+        unflipCard();
+    }
+}
+
+function disbleCards(){
+    firstCard.removeEventListner('click', flipCard);
+}
+
+function unflipCard(){
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+
+        lockBoard = false;
+    }, 1500);
+
 }
 
 cards.forEach(card => {
